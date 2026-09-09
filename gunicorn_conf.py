6a18478c01688ee48108d4ab70adc918adc16a40
jsando-ciatec <158347854+jsando-ciatec@users.noreply.github.com>
2026-09-09 10:31:56 -0600

    update

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,14 +1,7 @@
-from multiprocessing import cpu_count
-
-# Socket Path
-bind = 'unix:/home/ec2-user/SERVICOS/API_charly/gunicorn.sock'
-
-# Worker Options
-workers = cpu_count() + 1
-worker_class = 'uvicorn.workers.UvicornWorker'
-
-# Logging Options
-loglevel = 'debug'
-accesslog = '/home/ec2-user/SERVICOS/API_charly/access_log'
-errorlog =  '/home/ec2-user/SERVICOS/API_charly/error_log'
+bind = "0.0.0.0:8008"
+workers = 2
+worker_class = "uvicorn.workers.UvicornWorker"
+loglevel = "info"
+accesslog = "-"
+errorlog = "-"
 
